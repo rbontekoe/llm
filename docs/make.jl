ed628b427546199c623f4df145051f657e6e452e
rbontekoe <rbontekoe@appligate.nl>
2025-02-06 14:20:33 +0100

    update

--- docs/make.jl
+++ docs/make.jl
@@ -9,10 +9,9 @@ makedocs(
         "Blog" => "blog.md",
         "Sectie - Installation" => [
             "1 Install Docker" => "chapter1.md",
-            "2 Install the MiniConda container" => "chapter2.md",
-            "3 Start the MiniConda container" => "chapter3.md",
-            "4 How to get ChatGPT's API key" => "chapter4.md",
-            "5 Create the `.env` file for API keys" => "chapter5.md"
+            "2 Start the MiniConda container" => "chapter3.md",
+            "3 How to get ChatGPT's API key" => "chapter4.md",
+            "4 Create the `.env` file for API keys" => "chapter5.md"
         ],
         "Appendix" => "appendix.md",
         "References" => "references.md"
